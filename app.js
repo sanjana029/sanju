887d20801ae6406cd94a316c20b6cf54d4dbe01b
sanjana <sanjanamanwatkar58@gmail.com>
2022-06-14 11:37:19 +0530

    change

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var formRouter = require('./routes/form');
 
 var usersRouter = require('./routes/users');
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/form', formRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
